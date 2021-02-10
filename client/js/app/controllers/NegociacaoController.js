722d9e925210832057fda22792716fef85a9222a
itallosavieira <itallo.sa.vieira@gmail.com>
2021-02-10 18:52:05 -0300

    add Controller

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -1,3 +1,27 @@
-class NegocioController {
-    
+class NegociacaoController {
+
+    constructor() {
+
+        const $ = document.querySelector.bind(document);
+        this._inputData = $('#data');
+        this._inputQuantidade = $('#quantidade');
+        this._inputValor = $("#valor");
+    }
+
+    adiciona(event) {
+
+        event.preventDefault();
+
+        const data = new Date(this._inputData.value.split('-'));
+
+        let negocio = new Negociacao(
+            data,
+            this._inputQuantidade.value,
+            this._inputValor.value
+        );
+
+        console.log(negocio)
+    }
+
+
 }
